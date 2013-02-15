37ae62c57dd8ff5f391e424087f0dd536f6ee612
Sho Hashimoto <hashimoto@shokai.org>
2013-02-16 01:58:00 +0900

    add seek button

--- sample/public/js/remote.js
+++ sample/public/js/remote.js
@@ -4,10 +4,18 @@ var tv = screen.tv;
 
 cometio.on("connect", function(session){
   console.log("connect!! "+session);
-});
-
-$(function(){
   $("#btn_play").click(function(e){
     tv.$("#btn_play").click();
   });
+
+  $("#btn_seek").click(function(e){
+    tv.$("#btn_seek").click();
+  });
+  $("#btn_seekback").click(function(e){
+    tv.$("#btn_seekback").click();
+  });
+
+});
+
+$(function(){
 });
