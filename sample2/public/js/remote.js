e283965513f237c4ae54efc1c9d48f1e773c5cd1
Sho Hashimoto <hashimoto@shokai.org>
2013-02-22 19:52:14 +0900

    generate code in sample #3

--- sample2/public/js/remote.js
+++ sample2/public/js/remote.js
@@ -7,10 +7,18 @@ io.on("connect", function(session){
 });
 
 $(function(){
+  generate_code();
+  $("select").change(generate_code);
+
   $("#btn_dispatch").click(function(e){
-    var selector = $("#selector").val();
-    var event_name = $('#event_name').val();
     // tv.$("#btn_red").click();
-    tv.$(selector)[event_name]();
+    // tv.$("#btn_blue").mouseover();
+    eval( $("#code").val() );
   });
 });
+
+var generate_code = function(){
+  var selector = $("#selector").val();
+  var event_name = $('#event_name').val();
+  $("#code").val( 'tv.$("'+selector+'").'+event_name+'();' );
+};
